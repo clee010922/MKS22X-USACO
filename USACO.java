c286758314ff8c37c1195703db779af8f11e7db8
clee010922 <clee01@stuy.edu>
2019-03-06 10:18:41 -0500

    progress in class on bronze problem

--- USACO.java
+++ USACO.java
@@ -1,6 +1,23 @@
+import java.util.*;
+import java.io.*;
 public class USACO {
 
   public static int bronze(String filename) {
+    File file = new File(filename);
+    Scanner scanner = new Scanner(file);
+    int answer = 0;
+    int r = 0;
+    int c = 0;
+    int e = 0;
+    int n = 0;
+    int r_c = 0;
+    int c_s = 0;
+    int d_s = 0;
+    int[][] pasture = new int[r][c];
+    while (scanner.hasNextLine()) {
+    String line = scanner.nextLine();
+     
+    }
 
   }
 
