07ddb76c305bee03a5d9fe248bb13b617e5e5391
Chong <clee010922@gmail.com>
2019-03-10 22:52:40 -0400

    completed the bronze problem. Now have to test if it works

--- USACO.java
+++ USACO.java
@@ -5,18 +5,45 @@ public class USACO {
   public static int bronze(String filename) throws FileNotFoundException {
     File file = new File(filename);
     Scanner scanner = new Scanner(file);
-    int r = scanner.nextInt();
-    int c = scanner.nextInt();
-    int e = scanner.nextInt();
-    int n = scanner.nextInt();
-    int r_c = 0;
-    int c_s = 0;
-    int d_s = 0;
-    int depth = 0;
-    int[][] pasture = new int[r][c];
-    while (scanner.hasNextLine()) {
     String line = scanner.nextLine();
-
+    String[] variables = line.split(" ", -1);
+    int r = Integer.parseInt(variables[0]);
+    int c = Integer.parseInt(variables[1]);
+    int e = Integer.parseInt(variables[2]);
+    int n = Integer.parseInt(variables[3]);
+    int[][] stomps = new int[n][3];
+    int[][] pasture = new int[r][c];
+    for (int i = 0; i < r; i++) {
+      line = scanner.nextLine();
+      String[] pastureNumbers = line.split(" ", -1);
+      for (int j = 0; j < pastureNumbers.length; j++) {
+        pasture[i][j] = Integer.parseInt(pastureNumbers[j]);
+      }
+    }
+    for (int i = 0; i < n; i++) {
+      line = scanner.nextLine();
+      String[] stompInstructions = line.split(" ", -1);
+      for (int j = 0; i < stompInstructions.length; j++) {
+        stomps[i][j] = Integer.parseInt(stompInstructions[j]);
+      }
+    }
+    for (int i = 0; i < stomps.length; i++) {
+      int startRow = stomps[i][0];
+      int startCol = stomps[i][1];
+      int maxHeight = 0;
+      for (int r = startRow-1; r <= startRow+1; r++) {
+        for (int c = startCol-1; c <= startCol+1; c++) {
+          if (pasture[r][c] > maxHeight)
+            maxHeight = pasture[r][c];
+        }
+      }
+      maxHeight -= stomps[i][2];
+      for (int r = startRow-1; r <= startRow+1; r++) {
+        for (int c = startCol-1; c <= startCol+1; c++) {
+          if (pasture[r][c] > maxHeight)
+            pasture[r][c] = maxHeight;
+        }
+      }
     }
     //after the stomp is over
     for (int i = 0; i < r; i++) {
@@ -34,8 +61,8 @@ public class USACO {
     return depth * 72 * 72;
   }
 
-  public static int silver(String filename) {
+  //public static int silver(String filename) {
 
-  }
+  //}
 
 }
